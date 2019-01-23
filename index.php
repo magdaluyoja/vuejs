075ec113b6f057c934562978668ce536b6d2ef7f
Jay-R Magdaluyo <j.magdaluyo@seiko-it.com.ph.local>
2019-01-23 13:51:24 +0800

    Hello VueJs

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>VueJs</title>
+</head>
+<body>
+	<div id="app">
+	  	<p>{{ message }}</p>
+	</div>
+	<script src="assets/dist/js/app.min.js"></script>
+</body>
+</html>
